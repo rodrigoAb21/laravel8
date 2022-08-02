cca8bdd5dd2bb2f0f7fceabb8749874a8b36eb97
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2022-08-02 02:42:34 -0400

    Auth Sanctum API

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->group(function () {
+Route::post('/login', 'Api\AuthController@login');
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', 'Api\AuthController@logout');
+    Route::get('/user', 'Api\AuthController@user');
+    Route::resource('/trabajadores', 'Api\TrabajadorController');
 });
-Route::resource('trabajadores', 'Api\TrabajadorController');
-
